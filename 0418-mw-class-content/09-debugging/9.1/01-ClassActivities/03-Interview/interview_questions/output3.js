2f77f537845de1334f482c8241005bf433697441
Roger Le <rogermle@gmail.com>
2016-06-23 14:03:50 -0500

    Sync of Files between M/W and T/TH classes

---
+++ 0418-mw-class-content/09-debugging/9.1/01-ClassActivities/03-Interview/interview_questions/output3.js
@@ -0,0 +1,49 @@
+//What would the following lines of code output to the console?
+
+console.log("0 || 1 = "+(0 || 1));
+console.log("1 || 2 = "+(1 || 2));
+console.log("0 && 1 = "+(0 && 1));
+console.log("1 && 2 = "+(1 && 2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//solution
+
+//The code will output the following four lines:
+
+/*
+0 || 1 = 1
+1 || 2 = 1
+0 && 1 = 0
+1 && 2 = 2
+*/
