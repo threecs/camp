888f38b05b1f1c81f31d8c23c85ca6152b79323b
Roger Le <rogermle@gmail.com>
2016-08-29 13:25:48 -0500

    Adding week 20 React Native

---
+++ class-content/20-react-native/1-Class-Content/20.1/Activities/5-form/Skeleton/index.android.js
@@ -0,0 +1,231 @@
+/**
+ * Form Exercise 
+ **/
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  // Notice these imports:
+  TextInput,
+  TouchableOpacity
+
+} from 'react-native';
+
+class form extends Component {
+
+  // We make a constructor to initialize our states.
+  constructor() {
+    super();
+    this.state = {
+      message: '',
+      revealed: false,
+      inputText: ''
+    }
+  }
+
+  // This will set our message to the input text
+  // and then change input text to a blank string.
+  _setMessage() {
+    // TODO
+    // ====
+    // Write the contents of this function. 
+    // It should set the message state to the value of the TextInput
+    // and set the TextInput's Value to a blank string.
+  }
+
+  // This will toggle the display of our message box.
+  _toggleDisplay() {
+    var revealed = this.state.revealed;
+    this.setState({
+      // TODO
+      // ====
+      // Toggle the value of the state
+      // that controls whether the message is visible.
+    })
+  }
+
+  // Our render function.
+  render() {
+    return (
+      // Container
+      <View style={styles.container}>
+
+        {/* Header */}
+        <View style={styles.header}>
+          <Text style={styles.headerText}>State Changer!</Text>
+        </View>
+
+        {/* Our TextInput container.*/}
+
+        <View style={styles.i_container}>
+
+        {/* TODO
+          * ====
+          * Write out the TextInput component. 
+          * Make the value prop equal the inputText state.
+          * Set the placeholder prop to "Enter Message Here!".
+          * Setup the onTextChange prop to change the property
+          * of inputText to whatever characters the user enters.
+          *
+          * PROTIP: You'll find a rundown of the textInput component here!
+          * https://facebook.github.io/react-native/docs/textinput.html
+          * 
+          * Below, you'll find button that will set 
+          * the message state to the input box's value.
+          * You just need to write the function that alters the state.
+          *
+          */}
+
+          <TouchableOpacity style={styles.setter} onPress={this._setMessage.bind(this)}>
+            <Text style={styles.setterText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* The Message Container */}
+        <View style={styles.m_container}>
+
+          {/* TODO
+            * ====
+            * Create a Message Revealer button using TouchableOpacity.
+            * Connect the _toggleDisplay function to the onPress prop,
+            * and don't forget to add .bind(this) to the function call.
+            * Use styles.revealer for TouchableOpacity
+            * and styles.revealerText for the button's text.
+            *
+            * BONUS: Change the text of the button to match whether pressing it
+            * will reveal the message or hide it.
+            *
+            */}
+
+          {/* The Message Subcontainer (holds the actual message) */}
+
+          {/* Notice how we only show this if the revealed state is true. */}
+          {this.state.revealed && 
+            (
+            <View style={styles.m_subContainer}>
+              <Text style={styles.m_text}>{this.state.message}</Text>
+            </View>
+            )
+          }
+        </View>
+
+        {/*Footer */}
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Copyright &copy;2016</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+// Our stylesheet
+const styles = StyleSheet.create({
+  
+  // Our primary container
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#F5FCFF'
+  },
+  
+  // Our header
+  header: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'crimson'
+  },
+  
+  // Our header text
+  headerText: {
+    fontSize: 36,
+    color: 'white'
+  },
+  
+  // Our input container
+  i_container: {
+    flex: 4,
+    margin: 10,
+    marginBottom: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'crimson'
+  },
+  
+  // Our textInput
+  textInput: {
+    marginLeft:10,
+    marginRight:10,
+    height: 40,
+    width: 300,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e2e2e2'
+  },
+
+  // Our setter button
+  setter: {
+    marginTop: 10,
+    padding: 10,
+    backgroundColor: 'red',
+    borderRadius: 5
+  },
+  
+  // The text of our setter button
+  setterText:{
+    fontSize: 16,
+    color: 'white'
+  },
+  
+  // Our message container
+  m_container: {
+    flex: 1,
+    margin: 10,
+    backgroundColor: 'crimson'
+  },
+  
+  // Our message revealer button
+  revealer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+  // The text of our message revealer button
+  revealerText: {
+    color: 'white',
+    padding: 3,
+    fontWeight: '600'
+  },
+  
+  // Our message sub-container (which houses the actual message)
+  m_subContainer: {
+    flex: 4,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  
+  // The text of the message
+  m_text: {
+    color: 'white',
+    fontSize: 20
+  },
+  
+  // Our footer
+  footer: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'crimson'
+  },
+
+  // The text in our footer
+  footerText: {
+    color: 'white'
+  }
+});
+
+AppRegistry.registerComponent('form', () => form);
---
+++ class-content/20-react-native/StudentGuide.md
@@ -0,0 +1,21 @@
+## Week 20 - React Native
+
+### Overview
+In this week, we'll explore the React Native framework. This spin on Facebook's React library lets developers create iOS and Android apps, but the learning curve is steep -- be prepared to spend extra time studying the syntax and makeup of a standard React Native project. We'll end the week with a day dedicated to final projects.
+
+### Key Activities 
+* [20.1 - Flex2](1-Class-Content/20.1/Activities/2-flex2)
+* [20.1 - ScrollView](1-Class-Content/20.1/Activities/4-scrollView)
+* [20.1 - Form with State](1-Class-Content/20.1/Activities/5-form)
+* [20.2 - Movie Search App](1-Class-Content/20.2/Activities/1-MovieSearchApp)
+
+### Helpful Links:
+* [React Native 0.30 Documentation](http://facebook.github.io/react-native/releases/0.30/) 
+* [NPM page for rninit](https://www.npmjs.com/package/rninit)
+* [Windows Installation Guide for React Native](1-Class-Content/21.1/Supplemental/React-Native-Basic-Guide-Win.pdf)
+* [MacOS Installation Guide for React Native](1-Class-Content/21.1/Supplemental/React-Native-Basic-Guide-Mac.pdf)
+
+-------
+
+### Copyright 
+Coding Boot Camp (C) 2016. All Rights Reserved.
